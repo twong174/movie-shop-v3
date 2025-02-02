75b58eb66b095c8e837e91ca84c474727dd00b9e
twong174 <twong33@fordham.edu>
2025-02-02 15:29:21 -0500

    Backend

--- frontend/components/pages/LoginPage.jsx
+++ frontend/components/pages/LoginPage.jsx
@@ -1,7 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext";
+
 
 const LoginPage = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useContext(AuthContext);
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
+
   return (
     <div className="w-full h-screen p-10 bg-gray-100 flex items-center justify-center">
       <div className="bg-white w-1/2 h-full rounded-lg flex flex-col items-center justify-center">
@@ -12,15 +24,17 @@ const LoginPage = () => {
             <input
               className="text-sm placeholder-black border-b p-2 w-full mt-2 focus:outline-none"
               placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
             />
 
             <input
               className=" text-sm placeholder-black border-b p-2 w-full mt-2 focus:outline-none"
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
             />
           </div>
-          <button className="cursor-pointer text-sm rounded-xl bg-black text-white w-full p-2 mt-4">
+          <button className="cursor-pointer text-sm rounded-xl bg-black text-white w-full p-2 mt-4" onClick = {handleClick}>
             Login
           </button>
           <p className="text-xs mt-15">
